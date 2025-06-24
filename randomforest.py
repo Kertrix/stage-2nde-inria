d72a72dade9f2bf19eee1d5b724dddf9f832cdad
Tristan Kermorvant <tristan.kermorvant@gmail.com>
2025-06-24 16:08:00 +0200

    fixme

--- randomforest.py
+++ randomforest.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+from utils.pipelines import Pipelines
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
@@ -27,29 +29,31 @@ from utils.neural_utils import NeuralNetwork, predict
 
 ethnicities = ["Amerindian", "Asian", "Black", "Hispanic", "Mexican", "Other", "Puertorican", "White"]
 
-X_train_data = pd.read_csv("../law_data.csv")
+X_train_data = pd.read_csv("law_data.csv")
 y_train_data = X_train_data.pop("first_pf")
 
-X_train_data_encoded = pd.get_dummies(data=X_train_data.drop(columns=["race"]))
+A = X_train_data["race"]
 
+X_train, X_test, y_train, y_test, sf_train, sf_test = train_test_split(X_train_data, y_train_data, A, test_size=0.3, random_state=42, shuffle=True)
 
-X_train, X_test, y_train, y_test = train_test_split(X_train_data_encoded, y_train_data, test_size=0.3, random_state=42, shuffle=False)
-
+print(X_test.shape)
 #%%
-neural_network = NeuralNetwork(input_size=X_train.shape[1])
-# neural_network.load_state_dict(torch.load('neural_net/model.pth'))
+neural_network = NeuralNetwork(input_size=25)
+neural_network.load_state_dict(torch.load('neural_net/model.pth', map_location=torch.device('cpu')))
 #%%
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+rf = Pipelines(X_train.columns).random_forest()
+rf.fit(X_train, y_train)
 
-regressor = LogisticRegression(max_iter=1000, random_state=42)
+regressor = Pipelines(X_train.columns).regressor()
 regressor.fit(X_train, y_train)
 
 #%%
+print(X_test.shape)
+
 predictions = {
-    "rf": model.predict(X_test),
+    "rf": rf.predict(X_test),
     "reg": regressor.predict(X_test),
-    # "nn": predict(neural_network, prepare_data(X_test))
+    "nn": predict(neural_network, prepare_data(X_test))
 }
 
 # print(classification_report(y_test, predictions["nn"]))
@@ -57,7 +61,7 @@ predictions = {
 compare(
     [classification_report(y_test, predictions["rf"], output_dict=True),
         classification_report(y_test, predictions["reg"], output_dict=True),
-        # classification_report(y_test, predictions["nn"], output_dict=True)
+        classification_report(y_test, predictions["nn"], output_dict=True)
         ],
         model_names=["Random Forest", "Logistic Regression", "Neural Network"],
 )
@@ -72,7 +76,7 @@ results_sex = {
 
 sex = X_test.groupby("sex")
 for name, groups in sex:
-    pred_rf = model.predict(groups)
+    pred_rf = rf.predict(groups)
     pred_reg = regressor.predict(groups)
     pred_nn = predict(neural_network, prepare_data(groups))
     
@@ -107,7 +111,7 @@ for ethnicity in ethnicities:
     group = X_test.groupby("race_"+ethnicity)
     for name, groups in group:
         if name == True:
-            pred_rf = model.predict(groups)
+            pred_rf = rf.predict(groups)
             pred_reg = regressor.predict(groups)
             pred_nn = predict(neural_network, prepare_data(groups))
             
@@ -142,7 +146,7 @@ for region in regions:
     group = X_test.groupby("region_first_"+region)
     for name, groups in group:
         if name == True:
-            pred = model.predict(groups)
+            pred = rf.predict(groups)
             pred_reg = regressor.predict(groups)
             pred_nn = predict(neural_network, prepare_data(groups))
             
@@ -170,23 +174,13 @@ for model_name, results in results_regions.items():
 # rf_model = RandomForestClassifier(random_state=1)
 # grid_search = GridSearchCV(rf_model, parameters, cv=5, n_jobs=-1, return_train_score=True, verbose=3)
 # grid_search.fit(X_train_data, y_train_data)
-# %%
-sf_data = X_train_data[X_train_data['idx'].isin(X_test['idx'])]
-
-assert len(sf_data) == len(X_test)
-assert sf_data['idx'].equals(X_test['idx'])
 
 #%%
 # demographic parity 
 
 print("Demographic Parity Metrics:  \nbased on ethinicity")
-print(demographic_parity_difference(y_test, predictions["reg"], sensitive_features=sf_data["race"]))
-print(demographic_parity_ratio(y_test, predictions["reg"], sensitive_features=sf_data["race"]))
-
-print("\nbased on sex")
-print(demographic_parity_difference(y_test, predictions["reg"], sensitive_features=sf_data["sex"]))
-print(demographic_parity_ratio(y_test, predictions["reg"], sensitive_features=sf_data["sex"]))
-
+print(demographic_parity_difference(y_test, predictions["rf"], sensitive_features=sf_test))
+print(demographic_parity_ratio(y_test, predictions["reg"], sensitive_features=sf_test))
 # %%
 # equalized odds
 print("Equalized Odds Metrics:  \nbased on ethinicity")
